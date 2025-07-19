e822c8a248387cdfc495ceccc86c8caff517b748
Daniel Pérez <dpdaniperezsanz@gmail.com>
2025-07-19 15:23:23 +0200

    Add content

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import {Link} from "react-router";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <Link to="/">
+                <p className="text-2xl font-bold">CVision</p>
+            </Link>
+            <Link to="/upload" className="primary-button w-fit">
+                <div className="font-bold">
+                    Subir CV
+                    </div>
+            </Link>
+        </nav>
+    )
+}
+export default Navbar
